37a60ea2b2e454b39143f1d3bf8f69f9677fbf55
ayush saini <ayushsaini16703@gmail.com>
2025-07-15 13:01:24 +0530

    forward ref

--- 12MegaBlog/src/App.jsx
+++ 12MegaBlog/src/App.jsx
@@ -1,15 +1,34 @@
-
+import {useDispatch} from 'react-redux';
+import { useEffect, useState } from 'react'
 import './App.css'
-
+import authService from './appwrite/auth';
+import { login, logout} from "./store/authSlice"
+import { Header } from './components/Header/Header.jsx';
+import { Footer } from './components/Footer/Footer.jsx';
+import { Outlet } from 'react-router-dom';
 function App() {
-  console.log(import.meta.env.VITE_APPWRITE_URL);
-  
+  const [loading,setLoading ] = useState(true)
+  const dispatch = useDispatch()
 
-  return (
-    <>
-      <h1>A blog app with Appwrite</h1>
-    </>
-  )
+  useEffect(()=>{
+    const user = authService.getCurrentUser()
+    .then((userData)=>{
+      if(userData) dispatch({userData})
+      else dispatch(logout())
+    })
+    .finally(()=> setLoading(false))
+  },[])
+return !loading ? (
+  <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
+    <div className='w-full block'>
+      <Header/>
+      <main>
+        TODO {/* <Outlet/> */}
+      </main>
+      <Footer/>
+    </div>
+  </div>
+) : (null)
 }
 
 export default App
